74bce3ed15d36bade1014e703fccfac16547be35
lillian <keynesiane@gmail.com>
2024-03-19 17:55:49 -0700

    rewrite in express

--- index.js
+++ index.js
@@ -1,59 +1,27 @@
-const http = require('node:http');
-const fs = require('fs').promises;
+const express = require("express");
+const app = express();
+const path = require('path');
 
-const host = 'localhost';
 const port = 8080;
-const ReqUrls = ["/index.html", "/about.html", "/contact-me.html",
-                "/404.html"];
-const pageContents = {
-    index: "",
-    about: "",
-    contact: "",
-    errorPage: ""
-    };
 
-const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    switch (req.url) {
-        case "/":
-            res.writeHead(200);
-            res.end(pageContents.index);
-            break;
-        case "/about":
-            res.writeHead(200);
-            res.end(pageContents.about);
-            break;
-        case "/contact-me":
-            res.writeHead(200);
-            res.end(pageContents.contact);
-            break;
-        default:
-            res.writeHead(404);
-            res.end(pageContents.errorPage);
-    }
-};
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+})
 
-const server = http.createServer(requestListener);
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, './about.html'));
+})
 
-async function readFiles(ReqUrls, host, port) {
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, './contact-me.html'));
+})
 
-    try {
-        pageContents.index = await fs.readFile(__dirname + ReqUrls[0]);
-        pageContents.about = await fs.readFile(__dirname + ReqUrls[1]);
-        pageContents.contact = await fs.readFile(__dirname + ReqUrls[2]);
-        pageContents.errorPage = await fs.readFile(__dirname + ReqUrls[3]);
-    }
-    catch (err) {
-        console.error(`Could not read ${ReqUrls[i]} file: ${err}`);
-        process.exit(1);
-    }
+app.get(/\/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, './404.html'));
+})
 
-    server.listen(port, host, () => {
-        console.log(`Server is running on http://${host}:${port}`);
-    });
-}
-
-readFiles(ReqUrls, host, port)
-
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}!`);
+});
 
 
